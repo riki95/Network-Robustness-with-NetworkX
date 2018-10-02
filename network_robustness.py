e0223f0a25ccf5bba25957cbe553c7ac87b624f4
Riccardo <riki-95@hotmail.it>
2018-10-02 15:41:22 +0200

    Plot Function that holds positions
    Found a Plot Function that holds fixed my nodes potision. In this way I can see the differences between graphs

--- network_robustness.py
+++ network_robustness.py
@@ -1,31 +1,51 @@
 import networkx as nx
-import random
+from random import random
 import matplotlib.pyplot as plt
+import pylab
 
 from Lab1.analyse_realistic_graph import do_computations
 
+g = nx.DiGraph()
+
 
 def remove_random_node(g, n=1):
+    import random
     for i in range(n):
         node = random.choice(list(g.node.keys()))
 
         g.remove_node(node)
 
-        draw_graph(g)
 
-
-def draw_graph(g):
-    nx.draw_networkx(g, node_size=50, font_size=2, arrowsize=3, node_color='b')
-    plt.draw()
-    plt.show()
+def create_graph():
+    with open('bitcoin.csv') as f:
+        for row in f:
+            s = row.split(',')
+            g.add_edge(s[0], s[1], weight=int(s[2]))
 
 
 def main():
     g = nx.DiGraph(nx.scale_free_graph(20))
     do_computations(g)
-    draw_graph(g)
     remove_random_node(g)
-    do_computations(g)
+
+    figure = plt.figure()
+    # Intact
+    G = nx.barabasi_albert_graph(10, 3)
+
+    node_pose = {}
+    for i in G.nodes():
+        node_pose[i] = (random(), random())
+
+    plt.subplot(121)
+    fig1 = nx.draw_networkx(G, pos=node_pose, fixed=node_pose.keys())
+
+    # Two nodes are removed
+    e = [4, 6]
+    G.remove_nodes_from(e)
+    plt.subplot(122)
+    fig2 = nx.draw_networkx(G, pos=node_pose, fixed=node_pose.keys())
+
+    plt.show()
 
 
 if __name__ == '__main__':
