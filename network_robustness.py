d15ee893d48921bdc2792e230910c461b6ed87f5
Riccardo <riki-95@hotmail.it>
2018-10-02 15:43:49 +0200

    Draw function moved to indipendent method

--- network_robustness.py
+++ network_robustness.py
@@ -9,7 +9,7 @@ g = nx.DiGraph()
 
 
 def remove_random_node(g, n=1):
-    import random
+    import random  # This because it crashes against the import at the top which works for the plot
     for i in range(n):
         node = random.choice(list(g.node.keys()))
 
@@ -23,29 +23,34 @@ def create_graph():
             g.add_edge(s[0], s[1], weight=int(s[2]))
 
 
+def draw_graph_fixed(g):
+    # Intact graph now
+    figure = plt.figure()
+
+    node_pose = {}
+    for i in g.nodes():
+        node_pose[i] = (random(), random())
+
+    plt.subplot(121)
+    fig1 = nx.draw_networkx(g, pos=node_pose, fixed=node_pose.keys())
+
+    # Two nodes are removed
+    e = [4, 6]
+    g.remove_nodes_from(e)
+    plt.subplot(122)
+    fig2 = nx.draw_networkx(g, pos=node_pose, fixed=node_pose.keys())
+
+    plt.show()
+
+
 def main():
     g = nx.DiGraph(nx.scale_free_graph(20))
     do_computations(g)
     remove_random_node(g)
 
-    figure = plt.figure()
-    # Intact
-    G = nx.barabasi_albert_graph(10, 3)
+    g = nx.barabasi_albert_graph(10, 3)
+    draw_graph_fixed(g)
 
-    node_pose = {}
-    for i in G.nodes():
-        node_pose[i] = (random(), random())
-
-    plt.subplot(121)
-    fig1 = nx.draw_networkx(G, pos=node_pose, fixed=node_pose.keys())
-
-    # Two nodes are removed
-    e = [4, 6]
-    G.remove_nodes_from(e)
-    plt.subplot(122)
-    fig2 = nx.draw_networkx(G, pos=node_pose, fixed=node_pose.keys())
-
-    plt.show()
 
 
 if __name__ == '__main__':
