5a2ca6d6ca95f0e7b7f964a584e6cfb9de9120f6
Riccardo <riki-95@hotmail.it>
2018-10-12 00:15:25 +0200

    All plots are working

--- network_robustness.py
+++ network_robustness.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import heapq
 
+
 from Lab1.analyse_realistic_graph import do_computations, create_graph, compute_centrality
 
 g = nx.Graph().to_undirected()
@@ -160,6 +161,7 @@ def bitcoin_robustness3():
     list_diameters_min = []
     list_giantcomponentnodes = []
 
+    print("::: Computing Degree :::")
     for x in range(1000):
         best_node, nodes = compute_centrality(g_remove_most_important, 'degree')
 
@@ -167,73 +169,142 @@ def bitcoin_robustness3():
         giant_comp = max(comps, key=len)
         comp_nodes_number = giant_comp.number_of_nodes()
         comp_len = len(comps)
-        if x == 10 or x == 20 or x == 30 or x == 40 or x == 50 or x == 60 or x == 70 or x == 80 or x == 90 or x == 100 or x == 200 \
-                or x == 300 or x == 400 or x == 500 or x == 600 or x == 700 or x == 900 or x == 1000:
+
+        if x == 10 or x == 100 or x == 200 or x == 300 or x == 400 or x == 500 or x == 600 or x == 700 or x == 900 \
+                or x == 1000:
             diameters = [nx.diameter(comp.to_undirected()) for comp in comps]
             list_diameters_max.append(max(diameters))
             list_diameters_min.append(min(diameters))
             list_diameters.append(sum(diameters) / len(diameters))
             list_diameters_number.append(x)
             list_giantcomponentnodes.append(comp_nodes_number)
+
         list_components.append(comp_len)
         list_removed.append(x)
 
         g_remove_most_important.remove_nodes_from([best_node])
 
-        comps = list(nx.connected_component_subgraphs(g_remove_most_important))
-        comp_len = len(comps)
-        list_components.append(comp_len)
-        list_removed.append(x)
-
     g_remove_most_important = g.copy()
     list_components2 = []
     list_giantcomponentnodes2 = []
+    list_diameters2 = []
+    list_diameters_number2 = []
+    list_diameters_max2 = []
+    list_diameters_min2 = []
 
+    print("::: Computing Closeness :::")
     for x in range(10):
         best_node, nodes = compute_centrality(g_remove_most_important, 'closeness')
         first_nodes = heapq.nlargest(100, nodes, key=nodes.get)
-        g_remove_most_important.remove_nodes_from(first_nodes)
 
         comps = list(nx.connected_component_subgraphs(g_remove_most_important))
         comp_len = len(comps)
         giant_comp = max(comps, key=len)
         comp_nodes_number = giant_comp.number_of_nodes()
+
+        diameters = [nx.diameter(comp.to_undirected()) for comp in comps]
+        list_diameters_max2.append(max(diameters))
+        list_diameters_min2.append(min(diameters))
+        list_diameters2.append(sum(diameters) / len(diameters))
+        list_diameters_number2.append(x)
+
         list_giantcomponentnodes2.append(comp_nodes_number)
         list_components2.append(comp_len)
+
+        g_remove_most_important.remove_nodes_from(first_nodes)
 
     g_remove_most_important = g.copy()
     list_components3 = []
     list_giantcomponentnodes3 = []
+    list_diameters3 = []
+    list_diameters_number3 = []
+    list_diameters_max3 = []
+    list_diameters_min3 = []
 
+    print("::: Computing Betweenness :::")
     for x in range(10):
         best_node, nodes = compute_centrality(g_remove_most_important, 'betweenness')
         first_nodes = heapq.nlargest(100, nodes, key=nodes.get)
-        g_remove_most_important.remove_nodes_from(first_nodes)
 
         comps = list(nx.connected_component_subgraphs(g_remove_most_important))
         comp_len = len(comps)
         giant_comp = max(comps, key=len)
         comp_nodes_number = giant_comp.number_of_nodes()
+
+        diameters = [nx.diameter(comp.to_undirected()) for comp in comps]
+        list_diameters_max3.append(max(diameters))
+        list_diameters_min3.append(min(diameters))
+        list_diameters3.append(sum(diameters) / len(diameters))
+        list_diameters_number3.append(x)
+
         list_giantcomponentnodes3.append(comp_nodes_number)
         list_components3.append(comp_len)
 
+        g_remove_most_important.remove_nodes_from(first_nodes)
+
+    print("::: Plotting :::")
+    # Plot of different centrality algo
     plt.plot(list_removed, list_components, 'r-')
     plt.plot(list_removed2, list_components2, 'b-')
     plt.plot(list_removed2, list_components3, 'g-')
-    plt.ylabel("Components")
-    plt.xlabel("Removed")
-    plt.legend(['Degree', 'Closeness', 'Betweenness'], loc='upper right')
+    plt.ylabel("Components number")
+    plt.xlabel("Removed nodes")
+    plt.legend(['Degree', 'Closeness', 'Betweenness'], loc='lower right')
+    plt.savefig('data/' + 'centralitiescomponents' + '.png', dpi=500)
     plt.show()
 
+    # Plot of Giant Component nodes number
+    plt.plot(list_diameters_number, list_giantcomponentnodes, 'r-')
+    plt.plot(list_removed2, list_giantcomponentnodes2, 'b-')
+    plt.plot(list_removed2, list_giantcomponentnodes3, 'g-')
+    plt.ylabel("Giant Component number of nodes")
+    plt.xlabel("Removed nodes")
+    plt.legend(['Degree', 'Closeness', 'Betweenness'], loc='lower right')
+    plt.savefig('data/' + 'giantcomponents' + '.png', dpi=500)
+    plt.show()
+
+    # Plot of degree diameters
     plt.plot(list_diameters_number, list_diameters_max, 'r-')
     plt.plot(list_diameters_number, list_diameters_min, 'b-')
     plt.plot(list_diameters_number, list_diameters, 'g-')
     plt.ylabel("Diameter")
-    plt.xlabel("Removed")
+    plt.xlabel("Removed nodes")
     plt.legend(['Max', 'Min', 'Average'], loc='upper right')
+    plt.savefig('data/' + 'diagram_degree' + '.png', dpi=500)
     plt.show()
 
-    plt.plot(list_diameters_number, list_giantcomponentnodes, 'y-')
+    # Plot of closeness diameters
+    plt.plot(list_diameters_number2, list_diameters_max2, 'r-')
+    plt.plot(list_diameters_number2, list_diameters_min2, 'b-')
+    plt.plot(list_diameters_number2, list_diameters2, 'g-')
+    plt.ylabel("Diameter")
+    plt.xlabel("Removed nodes")
+    plt.legend(['Max', 'Min', 'Average'], loc='upper right')
+    plt.savefig('data/' + 'diagram_closeness' + '.png', dpi=500)
+    plt.show()
+
+    # Plot of betweenness diameters
+    plt.plot(list_diameters_number3, list_diameters_max3, 'r-')
+    plt.plot(list_diameters_number3, list_diameters_min3, 'b-')
+    plt.plot(list_diameters_number3, list_diameters3, 'g-')
+    plt.ylabel("Diameter")
+    plt.xlabel("Removed nodes")
+    plt.legend(['Max', 'Min', 'Average'], loc='upper right')
+    plt.savefig('data/' + 'diagram_betweenness' + '.png', dpi=500)
+    plt.show()
+
+
+def prova():
+    create_graph()
+    g.to_undirected()
+    comps = list(nx.connected_component_subgraphs(g))
+    giant_comp = max(comps, key=len)  # This is a subgraph that we have to study
+    print('\tGiant component:')
+    print('\t\tNumber of nodes:', giant_comp.number_of_nodes())
+    print(type(giant_comp.number_of_nodes()))
+
+    print(giant_comp)
+    print(type(giant_comp))
 
 
 def main():
