0a21266747e721a76a6ce77e6375f4a9ad868179
Riccardo <riki-95@hotmail.it>
2018-10-16 23:04:20 +0200

    Updated Robustness

--- network_robustness.py
+++ network_robustness.py
@@ -7,9 +7,9 @@ from Lab1.analyse_realistic_graph import do_computations, create_graph, compute_
 
 g = nx.Graph().to_undirected()
 node_pose = {}
-drawGraphOn = False
-plotOn = False
-diametersOn = False
+drawGraphOn = True
+plotOn = True
+diametersOn = True
 plotDiametersOn = True
 
 
@@ -36,36 +36,6 @@ def draw_graph(g, layout, title, namefile):
         nx.draw_networkx(g, node_size=50, font_size=6, arrowsize=3, node_color='b', pos=layout)
     plt.savefig('data/' + namefile + '.png', dpi=500)
     plt.close()
-
-
-# when you do this remember do compute closeness on analysis
-def scale_free_robustness():
-    g = nx.DiGraph(nx.scale_free_graph(20)).to_undirected()
-
-    fixed_positions = {1: (0, 0), 2: (-1, 2)}  # dict with two of the positions set
-    fixed_nodes = fixed_positions.keys()
-    pos = nx.spring_layout(g, pos=fixed_positions, fixed=fixed_nodes)
-
-    print("Graph")
-    if drawGraphOn:
-        draw_graph(g, pos, "Graph", 'g1')
-    do_computations(g, 1)
-
-    print("Graph with 10 random nodes removed")
-    g_remove_random = g.copy()
-    remove_random_node(g_remove_random, 10)
-    if drawGraphOn:
-        draw_graph(g_remove_random, pos, "Graph with 10 random nodes removed", 'g2')
-    do_computations(g_remove_random, 2)
-
-    g_remove_most_important = g.copy()
-    for x in range(4):
-        best_node = compute_centrality(g_remove_most_important)
-        print("Removed best node: " + str(best_node))
-        g_remove_most_important.remove_nodes_from([best_node])
-        if drawGraphOn:
-            draw_graph(g_remove_most_important, pos, "Graph with node " + str(best_node) + " removed", 'g' + str(x+3))
-        do_computations(g_remove_most_important, x+3)
 
 
 # when you do this remember do compute degree on analysis
@@ -275,7 +245,8 @@ def bitcoin_robustness():
 
 def main():
     # scale_free_robustness()
-    bitcoin_robustness()
+    # sf_robustness()
+     bitcoin_robustness()
 
 
 if __name__ == '__main__':
